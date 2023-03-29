ef0a123c6e252e8d41fd992b3badd052855f8188
Thelma-Dev <osifoaisosa@gmail.com>
2023-03-29 13:24:05 -0500

    Added CustomerDetails Features

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using AdventureWorksAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AdventureWorksLt2019Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLT2019Context")));
@@ -13,6 +14,10 @@ app.MapGet("/customer", CustomerMethods.Read);
 app.MapPost("/customer/create", CustomerMethods.CreateCustomer);
 
 app.MapGet("/customer/delete", CustomerMethods.RemoveCustomer);
+
+app.MapPut("/customer/update", CustomerMethods.UpdateCustomer);
+
+app.MapGet("/customer/details", CustomerMethods.GetCustomerDetails);
 
 app.Run();
 
